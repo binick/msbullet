cb77a202d9a48f362a8876f9e97d6ab6dbca4513
Nicola Biancolini <nicola.biancolini@outlook.it>
2020-07-24 08:53:18 +0200

    setup stage

--- eng/setup.ps1
+++ eng/setup.ps1
@@ -1,5 +1,13 @@
-$version = Get-Content -Raw -Path ..\version.json | ConvertFrom-Json
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $RepoRoot
+)
 
+$versionPath = Join-Path $RepoRoot -ChildPath version.json
+    
+$version = Get-Content -Raw -Path $versionPath | ConvertFrom-Json
+    
 $IsPublicRelease = ($version.publicReleaseRefSpec | ForEach-Object { $branch -match $_ }) -contains $true
-
+    
 Write-Host "##vso[task.setvariable variable=_PublicRelease]$IsPublicRelease"
