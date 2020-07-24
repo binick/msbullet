afc4136444d20ea6458420478a14c8a0ea69bf45
Nicola Biancolini <nicola.biancolini@outlook.it>
2020-07-24 08:37:42 +0200

    setup stage

--- eng/setup.ps1
+++ eng/setup.ps1
@@ -1,4 +1,4 @@
-$version = Get-Content -Raw -Path ../version.json | ConvertFrom-Json
+$version = Get-Content -Raw -Path ..\version.json | ConvertFrom-Json
 
 $IsPublicRelease = ($version.publicReleaseRefSpec | ForEach-Object { $branch -match $_ }) -contains $true
 
