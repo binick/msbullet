9780ba7d03b4200dd05a3edd6e8521b3b3450928
Nicola Biancolini <nicola.biancolini@reactconsulting.it>
2020-07-24 09:18:21 +0200

    Fix setup script (#8)
    setup stage

---
+++ eng/setup.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $RepoRoot
+)
+
+$versionPath = Join-Path $RepoRoot -ChildPath version.json
+    
+$version = Get-Content -Raw -Path $versionPath | ConvertFrom-Json
+    
+$IsPublicRelease = ($version.publicReleaseRefSpec | ForEach-Object { $branch -match $_ }) -contains $true
+    
+Write-Host "##vso[task.setvariable variable=_PublicRelease]$IsPublicRelease"
